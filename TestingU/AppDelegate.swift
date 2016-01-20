4aca8f16c227b3cc17ea735f2a43958a01f94bec
rayshen <rayshen1012@gmail.com>
2016-01-20 14:44:40 +0800

    Initial Commit

---
+++ TestingU/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TestingU
+//
+//  Created by shen on 16/1/20.
+//  Copyright © 2016年 shen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
